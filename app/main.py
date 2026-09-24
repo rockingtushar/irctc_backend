20f38e3d69ab91d34f896ecf2393a5f3313c33c3
Tushar Agrawal <146312525+rockingtushar@users.noreply.github.com>
2026-09-25 01:00:49 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -180,6 +180,7 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://irctc-woad.vercel.app",
+    "https://irctc-mu.vercel.app",
 ]
 
 app.add_middleware(
